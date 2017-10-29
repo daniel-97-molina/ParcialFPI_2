ed85101e69d82b4bd0f5a623547126c7c310e696
root <root@axbay>
2017-10-29 10:29:37 -0600

    Reparando estructura

--- procesarPost.php
+++ procesarPost.php
@@ -3,7 +3,7 @@
 //collect data send as XML
 $xml = file_get_contents('php://input');
 //open a file handler with read and write permission
-$fh = fopen('data/usuarios.xml', 'w');
+$fh = fopen('data/usuarios2.xml', 'w');
 //writing XML string to the new file
 fwrite($fh, $xml);
 //closing the file handler

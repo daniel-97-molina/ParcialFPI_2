c3f1ac9fa0d5aaee0607a96c83f506eeee6d7cee
IrvinAyala <hypleyone@gmail.com>
2017-10-31 22:36:01 -0600

    Agrendo imagen usuario y nombre

--- scripts/generales.js
+++ scripts/generales.js
@@ -1,5 +1,5 @@
 function $(query) {
-  return document.querySelector(query);
+    return document.querySelector(query);
 }
 
 if (!localStorage.usuarioLogueado) {
@@ -8,19 +8,19 @@ if (!localStorage.usuarioLogueado) {
 }
 
 function validarInput(input, mensaje) {
-  if (input.validity.valueMissing) {
-    input.setCustomValidity(mensaje);
-  } else {
-    input.setCustomValidity("");
-  }
+    if (input.validity.valueMissing) {
+        input.setCustomValidity(mensaje);
+    } else {
+        input.setCustomValidity("");
+    }
 }
 
 
 function generarDivArticuloBig(nArticulo, sAutor, iPuntos) {
-  var aGenerado = document.createElement("a");
-  aGenerado.setAttribute("href", "postViewer.html?id="+nArticulo.getAttribute("idArticulo"));
-  aGenerado.className = "aContenedorArticulo";
-  aGenerado.innerHTML = `<div class="contenido">
+    var aGenerado = document.createElement("a");
+    aGenerado.setAttribute("href", "postViewer.html?id=" + nArticulo.getAttribute("idArticulo"));
+    aGenerado.className = "aContenedorArticulo";
+    aGenerado.innerHTML = `<div class="contenido">
       <div class="detalles">
         <p>${sAutor} - puntos: ${iPuntos}</p>
       </div>
@@ -28,27 +28,27 @@ function generarDivArticuloBig(nArticulo, sAutor, iPuntos) {
         <p>${nArticulo.getElementsByTagName("categoria")[0].childNodes[0].nodeValue}</p>
       </div>
       <img src="images/imagesArticulos/${nArticulo.getElementsByTagName("img")[0].childNodes[0].nodeValue}" alt="imagen-del-post">
-      <h3>${nArticulo.getElementsByTagName("titulo")[0].childNodes[0].nodeValue.substr(0,30)}</h3>
-      <p>${nArticulo.getElementsByTagName("contenido")[0].childNodes[0].nodeValue.substr(0,75)+"..."}</p>
+      <h3>${nArticulo.getElementsByTagName("titulo")[0].childNodes[0].nodeValue.substr(0, 30)}</h3>
+      <p>${nArticulo.getElementsByTagName("contenido")[0].childNodes[0].nodeValue.substr(0, 75) + "..."}</p>
     </div>
   `;
-  return aGenerado;
+    return aGenerado;
 }
 
 
-function generarDivArticuloSmall(nArticulo, sAutor, iPuntos){
-  var aGenerado = document.createElement("a");
-  aGenerado.setAttribute("href", "postViewer.html?id="+nArticulo.getAttribute("idArticulo"));
-  aGenerado.className = "aContenedorArticuloSmall";
-  aGenerado.innerHTML = `<div class="contenido">
+function generarDivArticuloSmall(nArticulo, sAutor, iPuntos) {
+    var aGenerado = document.createElement("a");
+    aGenerado.setAttribute("href", "postViewer.html?id=" + nArticulo.getAttribute("idArticulo"));
+    aGenerado.className = "aContenedorArticuloSmall";
+    aGenerado.innerHTML = `<div class="contenido">
       <img src="images/imagesArticulos/${nArticulo.getElementsByTagName("img")[0].childNodes[0].nodeValue}" alt="imagen-del-post"">
-      <h3>${nArticulo.getElementsByTagName("titulo")[0].childNodes[0].nodeValue.substr(0,30)}</h3>
+      <h3>${nArticulo.getElementsByTagName("titulo")[0].childNodes[0].nodeValue.substr(0, 30)}</h3>
       <div class="detalles">
         <p>${nArticulo.getElementsByTagName("categoria")[0].childNodes[0].nodeValue} - ${sAutor} - puntos: ${iPuntos}</p>
       </div>
     </div>
   `;
-  return aGenerado;
+    return aGenerado;
 }
 
 //DEVUELVE UN ARCHIVO XML - Parámetro: ruta del archivo
@@ -72,21 +72,21 @@ function generarDivArticuloSmall(nArticulo, sAutor, iPuntos){
 //  xmlhttp.send(documentoXML);
 //  }
 
-function Usuario(iId, sNombre, sCorreo, sPassword, cGenero, sImagen){
+function Usuario(iId, sNombre, sCorreo, sPassword, cGenero, sImagen) {
     this.correo = sCorreo;
     this.password = sPassword;
     this.imagen = sImagen;
     this.genero = cGenero;
-    Persona.call(this, iId ,sNombre);
-    
+    Persona.call(this, iId, sNombre);
+
 //    if(typeOf(Usuario.prototype.__iniciado) === "undefined"){
 //        Usuario.prototype.
 //        Usuario.prototype.__iniciado = true;
 //    }
-} 
+}
 
- 
-function Persona(iId, sNombre){
+
+function Persona(iId, sNombre) {
     this.id = iId;
     this.nombre = sNombre;
 }
@@ -103,7 +103,7 @@ function imagen(ruta) {//codigo para enviar la imagen al servidor
     let form = $("#form");
 
     var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("post", "imageHandler.php?ruta="+ruta);
+    xmlhttp.open("post", "imageHandler.php?ruta=" + ruta);
     xmlhttp.send(new FormData(form));
 }
 
@@ -134,3 +134,40 @@ $("#btnRegistrarme").onclick = function () {
     location.href = "registro.html";
 };
 
+
+
+//Para agregar imagen y nombre de usuario en el header
+
+window.onload = function () {
+//    if (!localStorage.usuarioLogueado) {
+//        location.href = "login.html";
+//    } else {
+        var x = location.href.split("id=")[1] || localStorage.usuarioLogueado;
+        var idUsuario = parseInt(x) - 1;
+        cargarXMLGenerales(idUsuario);
+//    }
+};
+
+function cargarXMLGenerales(idUsuario) {
+
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            cargarDatosGenerales(this, idUsuario);
+        }
+    };
+    xmlhttp.open("GET", "data/usuarios.xml", true);
+    xmlhttp.send();
+}
+
+
+function cargarDatosGenerales(xml) {
+    var xmlDocUsuarios = xml.responseXML;
+    var usuarios = xmlDocUsuarios.getElementsByTagName("usuario");
+
+    var usuarioActual = usuarios[localStorage.usuarioLogueado - 1];
+    $("#divUsuario img").setAttribute("src", "images/imagesPerfil/" + usuarioActual.getAttribute("imagen"));
+    $("#divUsuario h4").innerHTML = usuarioActual.getElementsByTagName("nombre")[0].childNodes[0].nodeValue;
+
+}
